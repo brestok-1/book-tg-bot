88575a1d5e76ce8d3c50f2850fac6e976b1455f5
maksim <6234513@gmail.com>
2023-06-22 23:35:28 +0300

    created api integration and book_handing functions

--- config_data/config.py
+++ config_data/config.py
@@ -16,3 +16,6 @@ def load_config(path: str | None = None) -> Config:
     env = Env()
     env.read_env(path)
     return Config(tg_bot=TgBot(token=env('BOT_TOKEN')))
+
+
+PAGE_SIZE = 1050
--- services/book_handing.py
+++ services/book_handing.py
@@ -1,9 +1,37 @@
-BOOK_SIZE = 1050
+import re
+from config_data.config import PAGE_SIZE
+from external_services.books_api import get_book_content
 
 
 def _get_part_text(text: str, start: int, size: int) -> tuple[str, int]:
-    pass
+    clean_text = _clear_text(text)[start:start + size + 1]
+    end_index = [i.end() for i in re.finditer(r'[,.!:;?]+', clean_text)]
+    final_index = end_index[-1] if end_index[-1] <= size else end_index[-2]
+    final_text = clean_text[:final_index]
+    return final_text, len(final_text)
 
 
-def prepare_book(content: str) -> None:
-    pass
+def _clear_text(text: str) -> str:
+    clean_text = re.sub(r'\[\d]', '', text)
+    return clean_text
+
+
+def prepare_book(content: str) -> dict[int:str]:
+    book_dict = {}
+    number = 1
+    text, end_index = _get_part_text(content, 0, PAGE_SIZE)
+    while True:
+        try:
+            book_dict[number] = text.strip()
+            text, index = _get_part_text(content, end_index, PAGE_SIZE)
+            end_index += index
+            number += 1
+        except Exception:
+            return book_dict
+
+
+if __name__ == '__main__':
+    text = get_book_content('kapitanskaya-dochka')
+    diction = prepare_book(text)
+    for k, v in diction.items():
+        print(f'{k} - {v}')
