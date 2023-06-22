daf6ac1b745865073bcd3cd83ded0df8ec6c2342
maksim <6234513@gmail.com>
2023-06-22 23:36:38 +0300

    created api integration and book_handing functions

---
+++ external_services/books_api.py
@@ -0,0 +1,36 @@
+import json
+
+import requests
+
+
+def get_all_books_title_slug() -> dict:
+    data = _get_api_data()
+    dict_title_slug = _parse_title_slug(data)
+    return dict_title_slug
+
+
+def _get_api_data() -> list[dict]:
+    response = requests.get('http://127.0.0.1:8000/api/books/')
+    return json.loads(response.text)
+
+
+def _parse_title_slug(data: list[dict]) -> dict:
+    dict_title_slug = {}
+    for i in data:
+        dict_title_slug[i['title']] = i['slug']
+    return dict_title_slug
+
+
+def get_book_content(slug: str) -> str:
+    content = _get_detail_api_data(slug)['content']
+    return content
+
+
+def _get_detail_api_data(slug: str) -> dict:
+    response = requests.get(f'http://127.0.0.1:8000/api/books/{slug}/')
+    return json.loads(response.text)
+
+
+if __name__ == '__main__':
+    print(get_all_books_title_slug())
+    print(get_book_content('kapitanskaya-dochka'))
